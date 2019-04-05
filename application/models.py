e548c044bc0a4380aaad6e992b23c5f378666605
Bulat <st054745@student.spbu.ru>
2019-04-05 22:31:54 +0300

    Add correct model

--- application/models.py
+++ application/models.py
@@ -1,59 +1,60 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=50)  # User name
-    email = models.EmailField(max_length=50)  # Email
-    password = models.CharField(max_length=50)  # Password
-    level = models.IntegerField(default=1)  # User level
-    map_id = models.IntegerField(default=0)  # Level of the map (add foreign key to MapLevel PK)
-    # photo = models.ImageField()  # User photo
-
-    def __str__(self):
-        return self.name + ' ' + self.email
-
-
-class MapLevel(models.Model):
-    level_name = models.CharField()
-    block_id = models.TextField()  # (add foreign key to Block PK)
+class Map(models.Model):
+    image = 0  # mock field
 
 
 class Block(models.Model):
-    map_id = models.IntegerField()  # (add foreign key to Map PK)
-    # image = models.ImageField()  # Block picture
+    map_id = models.ForeignKey(Map, on_delete=models.CASCADE, default=None)
+    image = 0  # mock field
 
 
-class Map(models.Model):
-    something = 0  # mock var
-    # image = models.ImageField()  # Map picture
+class MapLevel(models.Model):
+    level_name = models.CharField(max_length=50, default=None)
+    block_id = models.ForeignKey(Block, on_delete=models.CASCADE, default=None)
 
 
-# Need to be fixed
+class User(models.Model):
+    name = models.CharField(max_length=50, default=None)  # User name
+    email = models.EmailField(max_length=50, default=None)  # Email
+    password = models.CharField(max_length=50, default=None)  # Password
+    level = models.IntegerField(default=1)  # User level
+    map_level_id = models.ForeignKey(MapLevel, on_delete=models.CASCADE, default=None)
+    photo = 0  # mock field
+
+    def __str__(self):
+        return str(self.name) + str(self.email)
+
+
 class Task(models.Model):
-    something = 0  # mock var
+    score = models.IntegerField(default=0)
+    type = models.CharField(max_length=50, default=None)
+    description = models.TextField(default=None)
+    map_level_id = models.ForeignKey(MapLevel, on_delete=models.CASCADE, default=None)
 
 
 class Chat(models.Model):
-    task_id = models.IntegerField()  # (add foreign key to Task PK)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, default=None)
 
 
 class Solution(models.Model):
-    task_id = models.IntegerField()  # (add foreign key to Task PK)
-    user_id = models.IntegerField()  # (add foreign key to User PK)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, default=None)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
 
 class Theory(models.Model):
-    task_id = models.IntegerField()  # (add foreign key to Task PK)
-    text = models.TextField()
-    video = models.URLField()
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, default=None)
+    text = models.TextField(default=None)
+    video = models.URLField(default=None)
 
 
 class TaskType(models.Model):
-    task_id = models.IntegerField()  # (add foreign key to Task PK)
-    name_type = models.TextField()
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, default=None)
+    name_type = models.TextField(default=None)
 
 
 class ProgramTests(models.Model):
-    input = models.TextField()
-    output = models.TextField()
-    type_id = models.IntegerField()  # (add foreign key to TaskType PK)
+    input = models.TextField(default=None)
+    output = models.TextField(default=None)
+    type_id = models.ForeignKey(TaskType, on_delete=models.CASCADE, default=None)
