67cd3d3f2d744f23fcdd721318061831938d74e1
Bulat <st054745@student.spbu.ru>
2019-04-05 18:24:55 +0300

    Delete useless url, add before_script to travis

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,4 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
