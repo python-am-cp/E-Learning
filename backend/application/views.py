888128872510cee28be9fc42774600fa2f728516
VladaNills <vladarub@yandex.ru>
2019-04-09 22:32:21 +0300

    Vlada's fix

--- backend/application/views.py
+++ backend/application/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from application.models import User
+from rest_framework import generics
+from application.serializer import UserSerializer
 
-# Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
