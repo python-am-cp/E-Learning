354a62cc82461c7c4c8f24a6b3e75d2376c6e8b6
Bulat <st054745@student.spbu.ru>
2019-04-09 23:18:32 +0300

    Delete useless folder accounts

--- backend/application/views.py
+++ backend/application/views.py
@@ -1,7 +1,26 @@
+# for serializer
 from application.models import User
 from rest_framework import generics
 from application.serializer import UserSerializer
 
+# for signup function
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+# serializer registragion
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSerializer'
+	
+	
+# another one
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
