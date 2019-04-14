ab04b51668cb9bdc30b9c395239dfae99ffa0963
Snejanochka <Snejanochka@yandex.ru>
2019-04-14 20:52:54 +0300

    add Home page

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -5,15 +5,21 @@ import App from './App'
 import router from './router'
 import VueCookie from 'vue-cookie'
 import Vuetify from 'vuetify'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from '@/store'
 import Authentication from '@/components/pages/Authentication'
+//import First from '@/components/pages/Authentication'
 import 'vuetify/dist/vuetify.css'
+Vue.use(BootstrapVue)
 Vue.use(VueCookie)
 Vue.use(Vuetify)
 Vue.use(store)
 Vue.config.productionTip = false
 Authentication.checkAuthentication()
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
--- vueapp/src/router/index.js
+++ vueapp/src/router/index.js
@@ -4,6 +4,7 @@ import * as Auth from '@/components/pages/Authentication'
 // Pages
 import Home from '@/components/pages/Home/Home'
 import Authentication from '@/components/pages/Authentication/Authentication'
+import First from '@/components/pages/Authentication/First'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
@@ -17,13 +18,21 @@ const router = new Router({
       }
     },
     {
+      path: '/first',
+      name: 'First',
+      component: First,
+      meta: {
+        requiredAuth: true
+      }
+    },
+    {
       path: '/login',
       name: 'Authentication',
       component: Authentication
     }
   ]
 })
-router.beforeEach((to, from, next) => {
+/*router.beforeEach((to, from, next) => {
   if (to.meta.requiredAuth) {
     if (Auth.default.user.authenticated) {
       next()
@@ -34,4 +43,6 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
+
+ */
 export default router
