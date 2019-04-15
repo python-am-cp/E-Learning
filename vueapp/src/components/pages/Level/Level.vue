95686facfa933de5e6dee912c3d4ef1f6bdfb710
Snejanochka <Snejanochka@yandex.ru>
2019-04-15 14:59:06 +0300

    change Level.vue

--- vueapp/src/components/pages/Level/Level.vue
+++ vueapp/src/components/pages/Level/Level.vue
@@ -1,28 +1,44 @@
 <template>
-  <div class="Level-container">
+  <div class="Level-container" fixed = top>
     <b-navbar class="NB" fixed = top toggleable="sm">
       <b-navbar-brand class="NB-brand" href=""><span style="color:#FFFFFF">E-Learning</span></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <div class="course-nav main-nav__course-nav">
+      </div>
       <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="switch-nav">
+        <b-nav-form>
+
+        </b-nav-form>
+        <b-nav-item-dropdown text="Типы данных">
+          <b-dropdown-item href="#"><span style="color:#E0FFFF">EN</span></b-dropdown-item>
+          <b-dropdown-item href="#"><span style="color:#E0FFFF">ES</span></b-dropdown-item>
+          <b-dropdown-item href="#"><span style="color:#E0FFFF">RU</span></b-dropdown-item>
+          <b-dropdown-item href="#"><span style="color:#E0FFFF">FA</span></b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-form>
+
+        </b-nav-form>
+      </b-navbar-nav>
+
         <b-navbar-nav role="navigation" class="ml-auto">
           <b-nav-item class="nav-item" href="/"><span style="color:#E0FFFF">ELP</span></b-nav-item>
           <b-nav-item class="nav-item" href=""><span style="color:#E0FFFF">О ПРОЕКТЕ</span></b-nav-item>
-          <b-nav-item class="nav-item" href="login"><span style="color:#E0FFFF">ВХОД</span></b-nav-item>
-          <b-nav-item class="nav-item" href="level"><span style="color:#E0FFFF">НАЧАТЬ ОБУЧЕНИЕ</span></b-nav-item>
+          <b-nav-item class="nav-item" href="login"><span style="color:#E0FFFF">ВЫХОД</span></b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
    <article class="course-layout" fixed = top data-taskid="1">
      <div class="theory" >
-       <b-row>
-         <b-col cols="8">
+       <b-row class="text-center">
+         <b-col >
            <div class="main-theory-title">
              <h1 class="theory-title">Типы данных</h1>
            </div>
          </b-col>
-         <b-col cols="4">
+       </b-row>
+       <b-row class="text-right">
 
-         </b-col>
        </b-row>
      </div>
    </article>
@@ -37,8 +53,18 @@
     width: 100%;
     height: 100%;
   }
+  .Level-container{
+
+  }
   .theory{
     text-align: start;
+    width: 100%;
+    margin: 0 auto;
+    margin-left: 0%;
+    margin-right: 0%;
+  }
+  .switch-nav{
+    color: #FFFFFF;
   }
   //text-align : center
   .course-layout {
@@ -71,6 +97,8 @@
     color: black;
     font-size: x-large;
     text-align: center;
+    padding-bottom: 2%;
+    border-bottom: 1px solid rgba(100,100,100,.3)
   }
 
 </style>
